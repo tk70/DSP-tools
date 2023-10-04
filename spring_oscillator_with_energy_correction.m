606416e4eed0cdd4c1e5c16c9785513b04fdfb44
Jakub Turowski <jakub.turowski@nokia.com>
2023-10-05 00:08:52 +0200

    got rid of sqrt

--- spring_oscillator_with_energy_correction.m
+++ spring_oscillator_with_energy_correction.m
@@ -1,31 +1,45 @@
 T = 2;
-sr = 1000;
+sr =2000;
 dt = 1 / sr;
 N = sr * T;
 t = linspace(0, T, N);
 
-function [x, u, p1] = osc(dt, N, v0, Amp, f)
+function [x, u, p1, p2] = osc(dt, N, v0, Amp, f)
   v = v0;
   s = 0;
-  amp_max = Amp(1);
-  d = calculate_spring_osc_coeff(f(1));
+  f_1 = f(1);
   for i = 1:N
     x(i) = s;
     u(i) = v;
     p1(i) = s;
-    d_1 = d;
-    d = calculate_spring_osc_coeff(f(i));
-    if d_1 != 0
-       delta_d = d/d_1;
-      v *= sqrt(delta_d);
+
+    k = calculate_spring_osc_coeff(f(i));
+    if f_1 != 0
+       delta_f = f(i)/f_1;
+       v *= delta_f;
     endif
-    a = -d * s;
+
+     E = k*s*s + v*v;
+     Eset = Amp(i)^2*k;
+     control = Eset - E;
+     control = max(-0.05, control)*dt;
+     control = min(0.05, control)*dt;
+
+     v = v* (1+control);
+
+##    p1(i) = k*s*s;
+##    p2(i) = v*v;
+    p1(i) = E;
+    p2(i) = 1+control;
+
+    a = -k * s;
     v = v + a*dt;
     s = s + v*dt;
 
 
 
-    p1(i) = a;
+    f_1 = f(i);
+
   endfor
 endfunction
 
@@ -33,12 +47,21 @@ f1 = envelope(N,
 [    1 ,           2,           4,           5,           4,          5,           4],
 [ 0 0 ;        0 1;       1 1;     1 0.3; 0.3 0.3;       1 1;  0.2 0.2]);
 
+f1 = envelope(N,
+[    1],
+[ 1 1]);
+
+a1 = envelope(N,
+[    1],
+[ 1 1]);
+
 close all;
 
-[x1, u1, p1] = osc(dt, N, 2, a1, f1 .* 70);
+[x1, u1, p1, p2] = osc(dt, N, 1, a1, f1 .* 10);
 
 figure(1);
 hold on;
 plot(t, x1);
-plot(t, f1);
-
+#plot(t, f1);
+plot(t, p1);
+plot(t, p2);
