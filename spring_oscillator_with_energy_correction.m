7ea8d80bbed76f8ceb0148eb54201d96db4b6641
Jakub Turowski <jakubturowski92@.com>
2023-10-11 19:10:04 +0200

    control working

--- spring_oscillator_with_energy_correction.m
+++ spring_oscillator_with_energy_correction.m
@@ -1,8 +1,8 @@
 clear all;
 
-T = 4;
+T = 1;
 
-function [t, x, u, a_e, f_e, p1, p2] = osc(T, sr, v0, a_env, a, f_env, f, func)
+function [t, x, u, a_e, f_e, p, q] = osc(T, sr, v0, a_env, a, f_env, f, func)
   N = sr * T;
   dt = 1/sr;
   v = v0;
@@ -14,7 +14,6 @@ function [t, x, u, a_e, f_e, p1, p2] = osc(T, sr, v0, a_env, a, f_env, f, func)
   for i = 1:N
     x(i) = s;
     u(i) = v;
-    p1(i) = s;
 
     k = calculate_spring_osc_coeff(f(i));
     if f_1 != 0
@@ -23,27 +22,28 @@ function [t, x, u, a_e, f_e, p1, p2] = osc(T, sr, v0, a_env, a, f_env, f, func)
     endif
 
     E = k*s*s + v*v;
-    Amp2 = Amp(i)^2;
-    Eset = Amp2*k;
-    control = Eset - E;
-    Z = 0.9;
+    Eset = k*Amp(i)^2;
+    Emax = k; # k xmax^2, xmax = 1
+    e = (Eset - E)/Emax;
+    control = e;
+
+    Z = 0.99;
     control = max(-Z, control);
     control = min(Z, control);
-    control = func(control, control_1, dt);
-
-    v = v* (1+control);
-    s = s* (1+control);
-
-    p1(i) = E;
-    p2(i) = control * 10;
 
     a = -k * s;
     v = v + a*dt;
     s = s + v*dt;
 
-    f_1 = f(i);
+    vmax = 2*pi*f(i);
+    P = 1000;
+    r = vmax * control * sign(v) * P;
+    v = v + r * dt;
 
+    f_1 = f(i);
     control_1 = control;
+    p(i) = r;
+    q(i) = control*6000;
   endfor
 
   t = linspace(0, T, N);
@@ -52,29 +52,41 @@ function [t, x, u, a_e, f_e, p1, p2] = osc(T, sr, v0, a_env, a, f_env, f, func)
 endfunction
 
 f_env.sizes = ...
-  [    1]
+  [    1,   1];
 f_env.bounds = ...
-  [  0 1];
+  [  0 1; 0 0];
 
 a_env.sizes = ...
   [   1,   1,   1,   2,   0];
 a_env.bounds = ...
   [ 1 1; 0 0; 1 1; 0 2; 0 0];
 
-func1 = @(control, control_1, dt) (control + 0*control_1)/1 * 30 * dt;
-func2 = @(control, control_1, dt) (control + 1*control_1)/2 * 30 * dt;
-[t1, x1, u1, a_e1, f_e1, p1, q1] = osc(T, 100, 1, a_env, 1, f_env, 20, func1);
-[t2, x2, u2, a_e2, f_e2, p2, q2] = osc(T, 100, 1, a_env, 1, f_env, 20, func2);
+c8 = 4186.01;
+
+func1 = @(control, control_1, dt) 0;
+func2 = @(control, control_1, dt) 0;
+[t1, x1, u1, a_e1, f_e1, p1, q1] = osc(T, 44000, 0.01, a_env, 1, f_env, c8, func1);
+[t2, x2, u2, a_e2, f_e2, p2, q2] = osc(T, 96000, 0.01, a_env, 1, f_env, c8, func2);
 
 close all;
 
 figure(1);
-subplot(2, 1, 1);
+subplot(4, 1, 1);
   hold on;
   plot(t1, x1);
   plot(t1, a_e1);
 
-subplot(2, 1, 2);
+subplot(4, 1, 2);
+  hold on;
+  plot(t1, p1);
+  plot(t1, q1);
+
+subplot(4, 1, 3);
   hold on;
   plot(t2, x2);
   plot(t2, a_e2);
+
+subplot(4, 1, 4);
+  hold on;
+  plot(t2, p2);
+  plot(t2, q2);
